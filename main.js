3c2ade2a72e3425501602a628118b060e9c29f5b
Ishita Narta <ishitanarta@gmail.com>
2017-07-21 20:29:35 -0700

    project 3rd start

---
+++ main.js
@@ -0,0 +1,8 @@
+// Runs when HTML file is loaded
+$(document).ready(function() {
+   $('#user-email').on('input',function() {
+        var email = $('#user-email').val()
+        var message = 'Welcome Back, ' + email;
+        $('.welcome-message').text(message);
+    });
+});
