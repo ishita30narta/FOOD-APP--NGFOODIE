c170db51a2c16187a8abfcbfcf6036d0ab1b4990
Ishita Narta <ishitanarta@gmail.com>
2017-07-23 16:29:38 -0700

    add backgound image,go to main page,hide login page

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// Runs when HTML file is loaded
-$(document).ready(function() {
-   $('#user-email').on('input',function() {
-        var email = $('#user-email').val()
-        var message = 'Welcome Back, ' + email;
-        $('.welcome-message').text(message);
-    });
-});
+var foodieApp = angular.module('foodieApp',[]);
+  console.log(foodieApp);
+
+
+
+// chlo controller bnate h
+
+foodieApp.controller('mainController',function($scope) {
+   $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+})
