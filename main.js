37c0ae7e718d0e12fc210b914fbf954819e8c6a6
Ishita Narta <ishitanarta@gmail.com>
2017-07-30 20:23:33 -0700

    add comments in all the folders

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var foodieApp = angular.module('foodieApp',['ngRoute']);
+var foodieApp = angular.module('foodieApp',['ngRoute']); /*add super routing*/
   console.log(foodieApp);
 
 
@@ -7,8 +7,8 @@ var foodieApp = angular.module('foodieApp',['ngRoute']);
 //login page controller
 foodieApp.controller('loginController',function($scope) {
 })
-
-foodieApp.controller('mainController',function($scope) {
+//main page controller
+foodieApp.controller('mainController',function($scope) {/*1st restro ki informtion sari h*/
   name: 'Farzi Cafe',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
@@ -19,7 +19,7 @@ foodieApp.controller('mainController',function($scope) {
 	hours: '12 Noon to 1 AM (Mon-Sun)',
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
-{
+{/*2nd restro ki information sari h*/
 	name: 'Farzi Cafe',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
@@ -30,7 +30,7 @@ foodieApp.controller('mainController',function($scope) {
 	hours: '12 Noon to 1 AM (Mon-Sun)',
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
-{
+{/*3rd restro ki information sari h*/
 	name: 'Farzi Cafe',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
@@ -41,7 +41,7 @@ foodieApp.controller('mainController',function($scope) {
 	hours: '12 Noon to 1 AM (Mon-Sun)',
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
-{
+{/*4th restro ki information sari h*/
 	name: 'Farzi Cafe',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
